eb6c983ad3d0850ad2c37d4398338fbde5b675e5
Marjo <93marjo.ek@gmail.com>
2024-02-22 11:06:37 +0200

    Lisätty ominaisuuksia

--- src/main/java/projekti/bookstore/web/RestBookController.java
+++ src/main/java/projekti/bookstore/web/RestBookController.java
@@ -12,6 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -36,6 +41,16 @@ public class RestBookController {
         return repository.findById(id);
     }
     
+    @PostMapping("/book")
+    Book newBook(@RequestBody Book newBook) {
+        return repository.save(newBook);
+    }
+    
+    @PutMapping("/book/{id}")
+    Book editBook(@RequestBody Book editBook, @PathVariable Long id) {
+        editBook.setId(id);
+        return repository.save(editBook);
+    }
     
 
 }
